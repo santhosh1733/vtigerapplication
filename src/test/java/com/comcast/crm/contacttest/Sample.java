9afba67ada7723a10e8580412d85490420023db9
santhosh <santhosh@MSI>
2025-06-23 12:05:55 +0530

    newly added classes

---
+++ src/test/java/com/comcast/crm/contacttest/Sample.java
@@ -0,0 +1,5 @@
+package com.comcast.crm.contacttest;
+
+public class Sample {
+
+}
--- src/test/java/com/comcast/crm/product/CreatePurchaseOrderTest.java
+++ src/test/java/com/comcast/crm/product/CreatePurchaseOrderTest.java
@@ -1,9 +1,40 @@
 package com.comcast.crm.product;
 
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+
 import com.comcast.crm.basetest.Baseclass;
+import com.comcast.crm.pomclass.CerateNewProductPage;
+import com.comcast.crm.pomclass.HomePage;
+import com.comcast.crm.pomclass.VendorLookupPage;
+import com.comcast.crm.pomclass.productPage;
 
 public class CreatePurchaseOrderTest extends Baseclass{
-public void purchaseorder() {
+public void purchaseorder() throws EncryptedDocumentException, IOException, InterruptedException {
+	HomePage home=new HomePage(driver);
+	home.getProductslink().click();
 	
+	productPage productpg=new productPage(driver);
+	productpg.getnewproduct().click();
+	
+	CerateNewProductPage createproduct=new CerateNewProductPage(driver);
+	String productname = excel.getDataFromExcelSheet("product", 1, 0)+java.createRandomNumber();
+	createproduct.getProductnametextfield().sendKeys(productname);
+	createproduct.getproductcategorydropdown();
+	createproduct.getSalesstartdatetextfield();
+	createproduct.getSalesenddatetextfield();
+	String parentwindow = driver.getWindowHandle();
+	createproduct.getVendorlookup().click();
+	
+	String partialtext = "Vendors&action";
+	wd.switchToParentWindow(partialtext);
+	VendorLookupPage vendorlookup=new VendorLookupPage(driver);
+	int row = excel.getLastRow("vendorinfo");
+	String vendorname = excel.getDataFromExcelSheet("vendorinfo", row, 0);
+	vendorlookup.getVendorNametextfield().sendKeys(vendorname);
+	vendorlookup.getButton().click();
+	Thread.sleep(2000);
+	driver.close();
 }
 }
